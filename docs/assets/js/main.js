b40b5792752273522d49da1ce7a3a85328064c7f
Yok29 <104489419+Yoann-Kerguelen@users.noreply.github.com>
2022-08-06 01:13:36 +0200

    Version 1.0

---
+++ docs/assets/js/main.js
@@ -0,0 +1,150 @@
+var transparent = true;
+
+var navbar_initialized,
+    toggle_initialized = false;
+
+$(window).on('load', function () {
+    nowuiKit.initNavbarImage();
+
+    if ($('.navbar[color-on-scroll]').length != 0) {
+        nowuiKit.checkScrollForTransparentNavbar();
+        $(window).on('scroll', nowuiKit.checkScrollForTransparentNavbar)
+    }
+});
+
+$(document).ready(function () {
+    nowuiKit.initNavbarImage();
+
+    $navbar = $('.navbar[color-on-scroll]');
+    scroll_distance = $navbar.attr('color-on-scroll') || 500;
+
+    if ($('.navbar[color-on-scroll]').length != 0) {
+        nowuiKit.checkScrollForTransparentNavbar();
+        $(window).on('scroll', nowuiKit.checkScrollForTransparentNavbar)
+    }
+
+    if ($(window).width() >= 992) {
+        big_image = $('.page-header-image[data-parallax="true"]');
+
+        $(window).on('scroll', nowuiKitDemo.checkScrollForParallax);
+    }
+
+});
+
+$(window).on('resize', function () {
+    nowuiKit.initNavbarImage();
+});
+
+$(document).on('click', '.navbar-toggler', function () {
+    $toggle = $(this);
+
+    if (nowuiKit.misc.navbar_menu_visible == 1) {
+        $('html').removeClass('nav-open');
+        nowuiKit.misc.navbar_menu_visible = 0;
+        $('#bodyClick').remove();
+        setTimeout(function () {
+            $toggle.removeClass('toggled');
+        }, 550);
+    } else {
+        setTimeout(function () {
+            $toggle.addClass('toggled');
+        }, 580);
+        div = '<div id="bodyClick"></div>';
+        $(div).appendTo('body').click(function () {
+            $('html').removeClass('nav-open');
+            nowuiKit.misc.navbar_menu_visible = 0;
+            setTimeout(function () {
+                $toggle.removeClass('toggled');
+                $('#bodyClick').remove();
+            }, 550);
+        });
+
+        $('html').addClass('nav-open');
+        nowuiKit.misc.navbar_menu_visible = 1;
+    }
+});
+
+$(document).on('click', '.page-content', function () {
+    if (nowuiKit.misc.navbar_menu_visible == 1) {
+        $('html').removeClass('nav-open');
+        nowuiKit.misc.navbar_menu_visible = 0;
+        $('#bodyClick').remove();
+        setTimeout(function () {
+            $toggle.removeClass('toggled');
+        }, 550);
+    }
+});
+
+nowuiKit = {
+    misc: {
+        navbar_menu_visible: 0
+    },
+
+    checkScrollForTransparentNavbar: debounce(function () {
+        if ($(document).scrollTop() > scroll_distance) {
+            if (transparent) {
+                transparent = false;
+                $('.navbar[color-on-scroll]').removeClass('navbar-transparent');
+            }
+        } else {
+            if (!transparent) {
+                transparent = true;
+                $('.navbar[color-on-scroll]').addClass('navbar-transparent');
+            }
+        }
+    }, 17),
+
+    initNavbarImage: function () {
+        var $navbar = $('.navbar').find('.navbar-translate').siblings('.navbar-collapse');
+        var background_image = $navbar.data('nav-image');
+
+        if ($(window).width() < 991 || $('body').hasClass('burger-menu')) {
+            if (background_image != undefined) {
+                $navbar.css('background', "url('" + background_image + "')")
+                    .removeAttr('data-nav-image')
+                    .css('background-size', "cover")
+                    .addClass('has-image');
+            }
+        } else if (background_image != undefined) {
+            $navbar.css('background', "")
+                .attr('data-nav-image', '' + background_image + '')
+                .css('background-size', "")
+                .removeClass('has-image');
+        }
+    },
+
+}
+
+
+var big_image;
+
+nowuiKitDemo = {
+    checkScrollForParallax: debounce(function () {
+        var current_scroll = $(this).scrollTop();
+
+        oVal = ($(window).scrollTop() / 3);
+        big_image.css({
+            'transform': 'translate3d(0,' + oVal + 'px,0)',
+            '-webkit-transform': 'translate3d(0,' + oVal + 'px,0)',
+            '-ms-transform': 'translate3d(0,' + oVal + 'px,0)',
+            '-o-transform': 'translate3d(0,' + oVal + 'px,0)'
+        });
+
+    }, 6)
+
+}
+
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        }, wait);
+        if (immediate && !timeout) func.apply(context, args);
+    };
+};
